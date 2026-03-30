6b7f6c15d1885afefcbddbb43e13fd8fcbfacecb
akshay <akshaypanaganti9395@gmail.com>
2026-03-30 06:25:31 +0530

    addend env

--- surebackend/index.js
+++ surebackend/index.js
@@ -144,7 +144,7 @@ app.delete("/delete",async(req,res)=>{
 
 
 
-let port=5000
+let port=process.env.PORT || 5000
 app.listen(port,()=>{
     console.log(`server running on :http://localhost:${port}`);
     
